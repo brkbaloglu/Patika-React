3977311e618b4ad49b9855436b6b5aa901a1dc25
Mehmet Burak Baloğlu <burakbaloglu148@gmail.com>
2024-06-01 23:24:12 +0300

    progress

---
+++ rui/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      <button className='px-4 py-2 border-2 border-black rounded-xl hover:bg-black hover:text-white hover:scale-x-110 hover:scale-y-90 duration-300'>Click</button>
+    </div>
+  );
+}
+
+export default App;
